a55ad19893113b03c4dc84fbe34db1b398479025
Tarun Agrawal <91940003+tarunagrawal442@users.noreply.github.com>
2021-10-21 17:00:30 -0400

    Add files via upload

---
+++ Covid Data Analysis.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+data<-read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+data
+colnames(data)
+# Selecting columns for Total Cases
+data_t<- data %>% select(date,continent,location,population,total_cases)
+
+# Filtering data with mutating total cases as a percentage of total population
+data1<- data %>% select(date,continent,location,population,total_cases) %>% filter(!is.na(continent)) %>% mutate(tc_percentage=(total_cases/population)*100, na.rm=TRUE) %>% arrange(location)
+
+# Grouping by location which is country
+data2<- data1 %>% group_by(location) %>% summarise(popluation=mean(population,na.rm=TRUE),total_case=max(total_cases, na.rm = FALSE),tc_percentage=max(tc_percentage,na.rm = FALSE)) %>% arrange(tc_percentage)
+
+# Selecting the death data for the locations, and grouping by country
+death_data_country<- data %>% select(continent,location,total_deaths) %>% filter(!is.na(continent)) %>% group_by(location) %>% summarise(total_deaths=max(total_deaths, na.rm = TRUE)) %>% arrange(desc(total_deaths))
+
+# # Selecting the death data for the locations, and grouping by continent
+death_data_continent <- death_data_country<- data %>% select(continent,location,total_deaths) %>% filter(!is.na(continent)) %>% group_by(continent) %>% summarise(total_deaths=max(total_deaths, na.rm = TRUE)) %>% arrange(desc(total_deaths))
+
+# Daily cases total across the world 
+# keep in mind to remove the null continents
+newcases_daywise <- data %>% filter(!is.na(continent)) %>% select(date,new_cases,new_deaths) %>%  group_by(date) %>% summarise(new_case=sum(new_cases,na.rm=TRUE), deaths=sum(new_deaths, na.rm=TRUE))
+
+# Deaths as a percentage of total cases
+death_data_cases<- data %>% filter(!is.na(continent)) %>% select(date,continent,location,population,total_cases,total_deaths) %>% group_by(location) %>% summarise(case=max(total_cases,na.rm = TRUE), deaths=max(total_deaths,na.rm = TRUE))%>%mutate(death_percentage=(deaths/case)*100) %>% arrange(location)
+
+# Covid Vaccinations Data
+vaccinations<- data %>% filter(!is.na(continent)) %>% select(date,continent,location,population,new_vaccinations) %>% arrange(location,date)
+vaccination_total<- vaccinations %>% group_by(location) %>% mutate(total_vaccinations=cumsum(ifelse(is.na(new_vaccinations),0,new_vaccinations)))
+vaccination_total_percent<-vaccination_total %>% mutate(per_vaccinatatd=total_vaccinations/population*100)
+                                            
